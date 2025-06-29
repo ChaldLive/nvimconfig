fdc5a2f656b5642364f8e5d3883dec778b1823c0
Carsten Hald <chald.live@gmail.com>
2025-06-29 09:18:58 +0200

    Fixing up initialised stuff before branching into a develop

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,23 +3,41 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
-      debug = true,
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.rustywind,
+
+        -- Python
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+
+        -- Java
+        null_ls.builtins.formatting.google_java_format,
+
+        -- C#
+        null_ls.builtins.formatting.csharpier,
       },
       on_attach = function(client, bufnr)
+        -- Only set up auto-format if the LSP client supports formatting
         if client.supports_method("textDocument/formatting") then
+          -- Define a unique autocmd group for this buffer to avoid conflicts
+          local augroup = vim.api.nvim_create_augroup("FormatOnSave_" .. bufnr, { clear = true })
+
+          -- Set up format-on-save
           vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true }),
+            group = augroup,
             buffer = bufnr,
             callback = function()
               vim.lsp.buf.format({ async = false })
             end,
           })
+
+          -- Optional: Set buffer-local keymap to manually format
+          vim.keymap.set("n", "<leader>gf", function()
+            vim.lsp.buf.format({ async = true })
+          end, { buffer = bufnr, desc = "Format current buffer" })
         end
       end,
     })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
