3801bc9510f3307e9ae8edd4e147177d287da5a1
Carsten Hald <chald.live@gmail.com>
2025-07-03 15:11:56 +0200

    Changed neo tree not to show certaing hidden folder and files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -12,23 +12,27 @@ return {
 			popup_border_style = "rounded",
 			enable_git_status = true,
 			enable_diagnostics = true,
+			hide_gitignored = true,
 			filesystem = {
 				filtered_items = {
+					hide_by_name = {
+						".DS_Store",
+						".DS_Store/",
+						"Thumbs.db",
+						"desktop.ini",
+						".localized",
+					},
+					never_show = {
+						".DS_Store",
+						".DS_Store/",
+						".git",
+						".Trash",
+						".cache",
+						"__pycache__",
+						"node_modules",
+					},
 					visible = true,
 					hide_dotfiles = false,
-				},
-				hide_by_name = {
-					".DS_Store",
-					"Thumbs.db",
-					"desktop.ini",
-					".localized",
-				},
-				never_show = {
-					".git",
-					".Trash",
-					".cache",
-					"__pycache__",
-					"node_modules",
 				},
 				follow_current_file = true, -- Follow the currently active file
 				hijack_netrw_behavior = "open_current", -- NetRW replacement
