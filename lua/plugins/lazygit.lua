fb460b707a79a0d525eba83ca35f29d47e42e6cf
Carsten Hald <chald.live@gmail.com>
2025-07-03 21:34:32 +0200

    This is a new plugin for the health of the lazygit plugin nothing else.

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,16 +1,20 @@
 return {
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-    },
-  },
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+			{ "<leader>lf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit (current file)" },
+			{ "<leader>lc", "<cmd>LazyGitConfig<cr>", desc = "Edit Lazygit config" },
+			{ "<leader>lh", "<cmd>LazyGitFilter<cr>", desc = "Git history (project)" },
+			{ "<leader>lH", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "Git history (file)" },
+		},
+	},
 }
