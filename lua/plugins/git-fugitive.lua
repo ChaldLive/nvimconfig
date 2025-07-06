be9746e1d5d35b8f12f131fe95e9429947f4015c
Carsten Hald <chald.live@gmail.com>
2025-07-06 10:37:39 +0200

    Changed git fugitives to make it a tool for merge conflicts

--- lua/plugins/git-fugitive.lua
+++ lua/plugins/git-fugitive.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"tpope/vim-fugitive",
-		cmd = { "Git", "G" }, -- Lazy-load on these commands
-		-- keys = {
-		--     { "<leader>gs", ":Git status<CR>", desc = "Git Status" },
-		--     { "<leader>gc", ":Git commit<CR>", desc = "Git Commit" },
-		--     { "<leader>gp", ":Git push<CR>", desc = "Git Push" },
-		--     { "<leader>gl", ":Git pull<CR>", desc = "Git Pull" },
-		--   },
-	},
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+    keys = {
+      { "<leader>gs", "<cmd>Git<CR>",             desc = "Git status" },
+      { "<leader>gb", "<cmd>G blame<CR>",         desc = "Git blame" },
+      { "<leader>gd", "<cmd>Gdiffsplit<CR>",      desc = "Git diff (split)" },
+      { "<leader>gl", "<cmd>G log --oneline<CR>", desc = "Git log (oneline)" },
+    },
+  },
 }
