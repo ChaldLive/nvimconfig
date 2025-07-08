ef483c148622264ba6534512d85fd9d8ffa3ae79
Carsten Hald <chald.live@gmail.com>
2025-07-08 08:11:15 +0200

    Removed lazy-lock.json and updatet some keybindings

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,11 +1,23 @@
--- Key mappings configuration
-local map = vim.api.nvim_set_keymap
+-- keymap.lua
+
+-- Set leader key (ensure this matches your init.lua)
+vim.g.mapleader = " "
+
+-- Shorthand for mapping
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- Map 'jj' to exit insert mode
+-- 🛸 Insert Mode
 map("i", "jj", "<Esc>", opts)
 map("i", "jk", "<Esc>", opts)
 
--- Dismiss to many anoying messages when in error mode.
-vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { })
+-- 📤 Utility
+map("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" })
+map("n", "<C-n>", "<cmd>Neotree filesystem reveal left<CR>", opts)
+
+-- 🧪 LazyGit Mappings
+map("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "Open LazyGit" })
+map("n", "<leader>lf", "<cmd>LazyGitCurrentFile<cr>", { desc = "LazyGit (current file)" })
+map("n", "<leader>lc", "<cmd>LazyGitConfig<cr>", { desc = "Edit Lazygit config" })
+map("n", "<leader>lh", "<cmd>LazyGitFilter<cr>", { desc = "Git history (project)" })
+map("n", "<leader>lH", "<cmd>LazyGitFilterCurrentFile<cr>", { desc = "Git history (file)" })
